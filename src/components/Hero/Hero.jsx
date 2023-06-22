8366620548894eb6ef7beb6fb3336e7c547e1aa6
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-22 18:43:59 +0430

    importing logo to header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Hero.css";
+import Header from "./Header/Header";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="left-h">
-      <Header />
+      <Header/>
       </div>
       <div className="right-h">right side</div>
     </div>
