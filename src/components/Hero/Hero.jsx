55daa034174b5e1c2b7dc913642fb824427350b1
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-22 18:38:46 +0430

    creating header component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className="hero">
+      <div className="left-h">
+      <Header />
+      </div>
+      <div className="right-h">right side</div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
