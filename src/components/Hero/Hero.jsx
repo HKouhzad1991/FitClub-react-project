809dffa833b2fb462e5d0270f6c4f810ace3898d
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-22 20:39:49 +0430

    hero text continue

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,20 +12,24 @@ const Hero = () => {
           <div></div>
           <span>The Best fitness Club In The Town </span>
         </div>
+        {/* hero heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape</span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>ideal body</span>
+          </div>
+          <div className="span">
+            In here we will help you to shape and build your ideal body and live
+            up your life to fullest
+          </div>
+        </div>
       </div>
 
-      {/* hero heading */}
-      <div className="hero-text">
-        <div>
-          <span className="stroke-text">Shape</span>
-          <span>Your</span>
-        </div>
-        <div>
-        <span>ideal body</span>
-        </div>
-      </div>
       <div className="right-h">right side</div>
-      </div>
+    </div>
   );
 };
 
