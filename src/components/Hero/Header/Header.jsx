04f2c098ca59360365b63ab4b1ab0f1efc69f105
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-22 18:55:09 +0430

    header list

--- src/components/Hero/Header/Header.jsx
+++ src/components/Hero/Header/Header.jsx
@@ -5,7 +5,13 @@ import Logo from '../../../assets/logo.png'
 const Header = () => {
     return <div className="header">
     <img src={Logo} alt="" className="logo" />
-    
+    <ul>
+        <li>Home</li>
+        <li>Programs</li>
+        <li>Why Us</li>
+        <li>Plans</li>
+        <li>Testimonials</li>
+    </ul>
     </div>;
 };
 
