259e3bbf226449202c23dd4eb3f5ccab3ad58ea2
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-22 18:47:23 +0430

    fixing logo

--- src/components/Hero/Header/Header.jsx
+++ src/components/Hero/Header/Header.jsx
@@ -4,7 +4,7 @@ import Logo from '../../../assets/logo.png'
 
 const Header = () => {
     return <div className="header">
-    <img src={Logo} alt="" />
+    <img src={Logo} alt="" className="logo" />
     
     </div>;
 };
