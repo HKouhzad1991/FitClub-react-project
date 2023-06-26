3bff7f5825f3441c41ba0827e56b95d4975defe1
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-26 17:05:18 +0430

    programs header style

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./Programs.css";
 
 const Programs = () => {
-    return (
-        <div className="Programs" id="Programs">
-        
-            {/* header for programs */}
-        
-        </div>)
+  return (
+    <div className="Programs" id="Programs">
+      {/* header for programs */}
+      <div className="programs-header">
+        <span>Explore our </span>
+        <span>Programs</span>
+        <span>to shape you</span>
+      </div>
+    </div>
+  );
 };
 
 export default Programs;
