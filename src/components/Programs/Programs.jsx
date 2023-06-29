f70f9326b1c4e58c67da1aa089032dcd6d9bfff9
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-30 00:05:50 +0430

    importing rightarrow

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Programs.css";
-import { programData } from '../../data/programsData' ;
+import { programsData } from "../../data/programsData";
 
 const Programs = () => {
   return (
@@ -10,6 +10,19 @@ const Programs = () => {
         <span className="stroke-text">Explore our </span>
         <span>Programs</span>
         <span className="stroke-text">to shape you</span>
+      </div>
+
+      <div className="program-categories">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              <span>Join Now</span><img src={RightArrow} alt="" /></image>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
