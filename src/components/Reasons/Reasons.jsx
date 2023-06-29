1451232727ebaaba5dfca0ffc2ab145765c33095
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-06-30 02:12:35 +0430

    left-r template

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -18,7 +18,7 @@ const Reasons = () => {
         <img src={image3} alt="" />
         <img src={image4} alt="" />
       </div>
-      <div className="right-r"></div>
+      <div className="right-r">Right Side</div>
     </div>
   );
 };
