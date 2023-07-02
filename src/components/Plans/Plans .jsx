ba581c97e321d792efe60f6ae4326b380f2fc72c
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-02 13:30:56 +0430

    importing stroke text in program header

--- src/components/Plans/Plans .jsx
+++ src/components/Plans/Plans .jsx
@@ -3,10 +3,10 @@ import "./Plans.css";
 const Plans = () => {
   return (
     <div className="plans-container">
-      <div className="plans-header">
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className="programs-header" style={{ gap: "2rem" }}>
+        <span className="stroke-text">READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span className="stroke-text">NOW WITH US</span>
       </div>
     </div>
   );
