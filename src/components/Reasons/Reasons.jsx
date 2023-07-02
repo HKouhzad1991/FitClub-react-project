bd88d9925c9340b1dfdb016506de8f7896c97b73
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-02 13:19:28 +0430

    importing brands image

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -24,22 +24,32 @@ const Reasons = () => {
           <span className="stroke-text">why</span>
           <span> choose us?</span>
         </div>
-        <div></div>
+
         <div className="details-r">
-          <img src={tick} alt=""></img>
-          <span>OVER 140+ EXPERT COACHES</span>
+          <div>
+            <img src={tick} alt=""></img>
+            <span>OVER 140+ EXPERT COACHES</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>I FREE PROGRAM FOR NEW MEMBER</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>RELIABLE PARTNERS</span>
+          </div>
         </div>
-        <div>
-          <img src={tick} alt="" />
-          <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
-        </div>
-        <div>
-          <img src={tick} alt="" />
-          <span>I FREE PROGRAM FOR NEW MEMBER</span>
-        </div>
-        <div>
-          <img src={tick} alt="" />
-          <span>RELIABLE PARTNERS</span>
+        <span style={{ color: "var(--gray)", fontWeight: "normal" }}>
+          OUR PARTNERS
+        </span>
+        <div className="partners">
+          <img src={nb}  alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
         </div>
       </div>
     </div>
