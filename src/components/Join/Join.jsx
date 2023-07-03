818ee1ffc1e4a1f9df5bf3960f9fe2c66f63e20b
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-04 01:13:28 +0430

    right-j and left-j

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./Join.css";
 
 const Join = () => {
-  return <div></div>;
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j"></div>
+      <div className="right-j"></div>
+    </div>
+  );
 };
 
 export default Join;
