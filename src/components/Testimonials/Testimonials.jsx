0f322251a493c872452255b4620f70567bdcd48f
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-03 19:33:03 +0430

    importing review testimonial

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Testimonials.css";
+import { testimonialsData } from "../../data/testimonialsData";
 
 const Testimonials = () => {
   return (
@@ -7,7 +8,10 @@ const Testimonials = () => {
       <div className="left-t">
         <span>Testimonials</span>
         <span className="stroke-text">What they</span>
-        <span>say about us</span>
+              <span>say about us</span>
+              <span>
+              {testimonialsData[0].review}
+              </span>
       </div>
       <div className="right-t"></div>
     </div>
