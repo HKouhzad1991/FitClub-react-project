a60252d5653c581dc42860f2abe71ead43e77af3
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-03 16:28:59 +0430

    importing ticks

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
 import "./Plans.css";
-const Plans = () => {
+const Plans= () => {
   return (
     <div className="plans-container">
       <div className="programs-header" style={{ gap: "2rem" }}>
@@ -14,16 +15,18 @@ const Plans = () => {
 
       <div className="plans">
         {plansData.map((plan, i) => (
-          <div className="plan" key={i}>  
+          <div className="plan" key={i}>
             {plan.icon}
             <span>{plan.name}</span>
             <span>$ {plan.price}</span>
+
             <div className="features">
               {plan.features.map((feature, i) => (
                 <div className="feature">
-                <img src="" alt="" />
+                  <img src={whiteTick} alt="" />
+                  <span key={i}>{feature}</span>
                 </div>
-            ))} 
+              ))}
             </div>
           </div>
         ))}
