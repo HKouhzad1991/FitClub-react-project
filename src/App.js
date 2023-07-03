976441c97695971cb38271ee37a8077642e56099
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-03 16:33:52 +0430

    adding cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
-import Plans from "./components/Plans/Plans";
+import Plans from "./components/Plans/Plans ";
 import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
 function App() {
--- src/components/Plans/Plans .jsx
+++ src/components/Plans/Plans .jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { plansData } from "../../data/plansData";
 import whiteTick from "../../assets/whiteTick.png";
 import "./Plans.css";
-const Plans= () => {
+const Plans=() => {
   return (
     <div className="plans-container">
       <div className="programs-header" style={{ gap: "2rem" }}>
