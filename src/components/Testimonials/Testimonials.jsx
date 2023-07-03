3e8255446b9638bcdf9ec15b86efa25c7c2cdff9
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-03 23:36:40 +0430

    importing arrows

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -4,7 +4,7 @@ import { testimonialsData } from "../../data/testimonialsData";
 
 const Testimonials = () => {
   const [selected, setSelected] = useState(0);
-  const tLength = testimonialsData.atlength;
+  const tLength = testimonialsData.tlength;
   return (
     <div className="Testimonials">
       <div className="left-t">
@@ -13,11 +13,18 @@ const Testimonials = () => {
         <span>say about us</span>
         <span>{testimonialsData[selected].review}</span>
         <span>
-          <span style={{color:'var(--orange)'}}>{testimonialsData[selected].name}</span>
+          <span style={{ color: "var(--orange)" }}>
+            {testimonialsData[selected].name}
+          </span>
           {""}-{testimonialsData[selected].status}
         </span>
       </div>
-      <div className="right-t"></div>
+      <div className="right-t">
+              <img src={testimonialsData[selected].image} alt="" />
+              <div className="arrows">
+              <img src={leftarrow} alt="" />
+              </div>
+          </div>
     </div>
   );
 };
