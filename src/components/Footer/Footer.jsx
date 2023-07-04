550882db657095bb735e95f396f116be6b593b42
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-04 14:58:08 +0430

    social links div

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,12 +11,14 @@ const Footer = () => {
       <hr />
 
       <div className="footer">
-        <img src={Github} alt="" />
-        <img src={Instagram} alt="" />
-        <img src={Linkedin} alt="" />
-      </div>
-      <div className="logo-f">
-        <img src={Logo} alt="" />
+        <div className="social-links">
+          <img src={Github} alt="" />
+          <img src={Instagram} alt="" />
+          <img src={Linkedin} alt="" />
+        </div>
+        <div className="logo-f">
+          <img src={Logo} alt="" />
+        </div>
       </div>
     </div>
   );
