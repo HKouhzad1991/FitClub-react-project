61782ba59333e6ffc37e6bc49dcd204b46f794f0
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-04 14:02:04 +0430

    hr importing and font style fixing

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -4,7 +4,17 @@ import "./Join.css";
 const Join = () => {
   return (
     <div className="Join" id="join-us">
-      <div className="left-j"></div>
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span> LEVEL UP</span>
+        </div>
+        <div>
+          <span> YOUR BODY</span>
+          <span className="stroke-text"> WITH US</span>
+        </div>
+      </div>
       <div className="right-j"></div>
     </div>
   );
