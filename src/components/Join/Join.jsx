7f6cb9d60397935dd4b11a8cb415fb2ad22fa2c7
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-04 14:34:11 +0430

    copy me email service id from emailjs site

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -3,26 +3,26 @@ import "./Join.css";
 import emailjs from "@emailjs/browser";
 
 const Join = () => {
-  const form = useRef()
-   const sendEmail = (e) => {
-     e.preventDefault();
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-     emailjs
-       .sendForm(
-         "YOUR_SERVICE_ID",
-         "YOUR_TEMPLATE_ID",
-         form.current,
-         "YOUR_PUBLIC_KEY"
-       )
-       .then(
-         (result) => {
-           console.log(result.text);
-         },
-         (error) => {
-           console.log(error.text);
-         }
-       );
-   };
+    emailjs
+      .sendForm(
+        "service_olvpt5x",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="Join" id="join-us">
       <div className="left-j">
@@ -37,7 +37,11 @@ const Join = () => {
         </div>
       </div>
       <div className="right-j">
-        <form ref={form} action="" className="email-container" onSubmit={sendEmail}>
+        <form
+          ref={form}
+          action=""
+          className="email-container"
+          onSubmit={sendEmail}>
           <input
             type="email"
             name="user_email"
