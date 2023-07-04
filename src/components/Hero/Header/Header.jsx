b00eead2bab6bf2002fdd7956a34b9e7d2e68153
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-04 23:52:43 +0430

    innerwidth importing for mobile or another screen

--- src/components/Hero/Header/Header.jsx
+++ src/components/Hero/Header/Header.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import "./Header.css";
-import Logo from '../../../assets/logo.png'
+import Logo from "../../../assets/logo.png";
 import Bars from "../../../assets/bars.png";
 
 const Header = () => {
-    return <div className="header">
-    <img src={Logo} alt="" className="logo" />
-    <ul className="header-menu">
+  const mobile = window.innerWidth < +768 ? true : false;
+  return (
+    <div className="header">
+      <img src={Logo} alt="" className="logo" />
+      <ul className="header-menu">
         <li>Home</li>
         <li>Programs</li>
         <li>Why Us</li>
         <li>Plans</li>
         <li>Testimonials</li>
-    </ul>
-    </div>;
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
