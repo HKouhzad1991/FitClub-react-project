9c97d56edaa0f51fd0a34fb5e1fac7b5d41bed10
https://github.com/HKouhzad1991/HKouhzad1991 <hkouhzad1991>
2023-07-04 16:18:49 +0430

    began animations and import motion framer to hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,6 +5,7 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
